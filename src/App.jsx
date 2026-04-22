ccfabca27abbf2941e49419ebc0c25a64dea429f
Mario Oliveira <m.eos2009@hotmail.com>
2026-04-22 10:25:28 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,38 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import AppLayout from "./layout/AppLayout";
-import ProtectedRoute from "./components/ProtectedRoute";
-
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-
+import Inicio from "./pages/Inicio";
 import Dashboard from "./pages/Dashboard";
 import Receitas from "./pages/Receitas";
 import Despesas from "./pages/Despesas";
+import ListaDespesas from "./pages/ListaDespesas";
 import Categorias from "./pages/Categorias";
 import RelatorioMensal from "./pages/RelatorioMensal";
 import RelatorioCategorias from "./pages/RelatorioCategorias";
-import ListaDespesas from "./pages/ListaDespesas";
-import Inicio from "./pages/Inicio";
+import Configuracoes from "./pages/Configuracoes";
 
 export default function App() {
   return (
-    <Routes>
+    <BrowserRouter>
+      <Routes>
 
-      {/* ROTAS PÚBLICAS */}
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-
-      {/* ROTAS PROTEGIDAS */}
-      <Route
-        element={
-          <ProtectedRoute>
-            <AppLayout />
-          </ProtectedRoute>
-        }
-      >
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/receitas" element={<Receitas />} />
-        <Route path="/despesas" element={<Despesas />} />
-        <Route path="/categorias" element={<Categorias />} />
-        <Route path="/relatorio-mensal" element={<RelatorioMensal />} />
-        <Route path="/relatorio-categorias" element={<RelatorioCategorias />} />
-        <Route path="/lista-despesas" element={<ListaDespesas />} />
+        {/* ⭐ PÁGINA INICIAL FORA DO LAYOUT */}
         <Route path="/" element={<Inicio />} />
         <Route path="/inicio" element={<Inicio />} />
-     
-</Route>
 
-    </Routes>
+        {/* ⭐ TODAS AS OUTRAS PÁGINAS DENTRO DO LAYOUT */}
+        <Route element={<AppLayout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/receitas" element={<Receitas />} />
+          <Route path="/despesas" element={<Despesas />} />
+          <Route path="/lista-despesas" element={<ListaDespesas />} />
+          <Route path="/categorias" element={<Categorias />} />
+          <Route path="/relatorio-mensal" element={<RelatorioMensal />} />
+          <Route path="/relatorio-categorias" element={<RelatorioCategorias />} />
+          <Route path="/configuracoes" element={<Configuracoes />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   );
 }
