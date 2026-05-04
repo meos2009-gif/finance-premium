a6317d67f016ffe39376b2325a151d1540f4ae42
Mario Oliveira <m.eos2009@hotmail.com>
2026-05-04 11:47:41 +0100

    update

--- src/pages/ListaDespesas.jsx
+++ src/pages/ListaDespesas.jsx
@@ -6,6 +6,8 @@ export default function ListaDespesas() {
   const [despesas, setDespesas] = useState([]);
   const [categorias, setCategorias] = useState([]);
   const [empresas, setEmpresas] = useState([]);
+  const [mes, setMes] = useState(new Date().getMonth() + 1);
+  const [ano, setAno] = useState(new Date().getFullYear());
 
   const [filtroCategoria, setFiltroCategoria] = useState("");
   const [filtroEmpresa, setFiltroEmpresa] = useState("");
@@ -161,10 +163,16 @@ export default function ListaDespesas() {
   };
 
   const despesasFiltradas = despesas.filter((d) => {
-    const matchCat = filtroCategoria ? d.category_id === filtroCategoria : true;
-    const matchEmp = filtroEmpresa ? d.empresa_id === filtroEmpresa : true;
-    return matchCat && matchEmp;
-  });
+  const data = new Date(d.date);
+  const mesTrans = data.getMonth() + 1;
+  const anoTrans = data.getFullYear();
+
+  const matchMesAno = mesTrans === Number(mes) && anoTrans === Number(ano);
+  const matchCat = filtroCategoria ? d.category_id === filtroCategoria : true;
+  const matchEmp = filtroEmpresa ? d.empresa_id === filtroEmpresa : true;
+
+  return matchMesAno && matchCat && matchEmp;
+});
 
   const totaisPorCategoria = Object.entries(
     despesasFiltradas.reduce((acc, d) => {
@@ -180,6 +188,41 @@ export default function ListaDespesas() {
         <h1 className="text-2xl font-bold text-[#facc15]">
           Lista de Despesas
         </h1>
+<div className="flex gap-4 bg-[#111] p-4 rounded-xl border border-[#222] w-full max-w-md">
+
+  {/* SELECT MÊS */}
+  <select
+    className="bg-[#222] p-3 rounded-lg w-full"
+    value={mes}
+    onChange={(e) => setMes(Number(e.target.value))}
+  >
+    <option value="1">Janeiro</option>
+    <option value="2">Fevereiro</option>
+    <option value="3">Março</option>
+    <option value="4">Abril</option>
+    <option value="5">Maio</option>
+    <option value="6">Junho</option>
+    <option value="7">Julho</option>
+    <option value="8">Agosto</option>
+    <option value="9">Setembro</option>
+    <option value="10">Outubro</option>
+    <option value="11">Novembro</option>
+    <option value="12">Dezembro</option>
+  </select>
+
+  {/* SELECT ANO */}
+  <select
+    className="bg-[#222] p-3 rounded-lg w-full"
+    value={ano}
+    onChange={(e) => setAno(Number(e.target.value))}
+  >
+    {Array.from({ length: 6 }).map((_, i) => {
+      const y = new Date().getFullYear() - i;
+      return <option key={y} value={y}>{y}</option>;
+    })}
+  </select>
+
+</div>
 
         <button
           onClick={() => setShowImportModal(true)}
