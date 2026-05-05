cd54c310ce46cee4e33dc9bf1ae6add48f5d9957
Mario Oliveira <m.eos2009@hotmail.com>
2026-05-05 17:18:03 +0100

    update

---
+++ src/utils/categoryIcons.js
@@ -0,0 +1,12 @@
+export const categoryIcons = {
+  Alimentação: "🍔",
+  Supermercado: "🛒",
+  Saúde: "💊",
+  Transporte: "🚗",
+  Casa: "🏠",
+  Lazer: "🎉",
+  Roupas: "👕",
+  Educação: "📚",
+  Tecnologia: "💻",
+  Outros: "📌",
+};
